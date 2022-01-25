58788713bfa5f8a0e8d681a515cbe108a2a6145f
trevin-livele <liveletrevin6@gmail.com>
2022-01-25 11:14:54 +0300

    removed function

--- js/script.js
+++ js/script.js
@@ -4,8 +4,3 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
 }
 
-let cartItem = document.querySelector('.cart-items-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    navbar.classList.toggle('active');
-}

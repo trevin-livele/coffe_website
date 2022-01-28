efc97310444e2b5c6894d02cfbe3edb9b2928ac9
trevin-livele <liveletrevin6@gmail.com>
2022-01-29 00:36:32 +0300

    created products and menu pages

--- js/script.js
+++ js/script.js
@@ -1,11 +1,29 @@
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector(".navbar");
 
-document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
-<<<<<<< HEAD
-}
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
 
+let searchForm = document.querySelector(".cart-items-container");
 
-=======
-}
->>>>>>> 7b3656045d8597313b6415054edf59f87cd87323
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let navbar = document.querySelector(".cart-items-container");
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
